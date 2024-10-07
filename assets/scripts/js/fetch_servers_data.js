1a1093a29dcc34807d146860703948241f27e6ac
tonkatsuki <6411441+tonkatsuki@users.noreply.github.com>
2024-10-06 22:55:32 -0500

    Update fetch_servers_data.js

--- assets/scripts/js/fetch_servers_data.js
+++ assets/scripts/js/fetch_servers_data.js
@@ -6,7 +6,7 @@ const icons = {
 
 // const gameOrder = ["Gmod", "VRising", "CS2"];
 const gameOrder = ["Gmod", "CS2"];
-const fetchUrl = "http://test3.tonkatsuki.com:27019/servers";
+const fetchUrl = "https://test3.tonkatsuki.com";
 
 async function fetchData(url) {
   try {
